37a705dea1645ef87416360a7594ed89d382d075
syed <71326007+SyedUmairAnwar@users.noreply.github.com>
2024-03-01 18:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1===""||s2===""){
+		return ''
+	}
+	let str=""
+	str= str+ s1.charAt(0);
+	str = str +s2.charAt(0);
+	return str;
 }
 
 // Do not change the code below.
